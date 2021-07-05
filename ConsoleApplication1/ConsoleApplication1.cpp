7858438fa7624fef604ec9313c14f454230fcfd7
stasv <stasv@DESKTOP-451E78B>
2021-07-05 14:02:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+     
+#include <iostream>
+    using namespace std;
+
+    // Функция вычисления среднего гармонического двух чисел
+    double sredgarm(double a, double b) {
+        return 2 / (1 / a + 1 / b);
+    }
+
+    int main() {
+        setlocale(LC_ALL, "Rus");
+        std::cout << "Задание №1" << std::endl;
+        // Ввод чисел
+        double a, b;
+        std::cout << "Введите два числа через пробел: ";
+        std::cin >> a >> b;
+        std::cout << "Результат вычисления среднего гармонического двух чисел: " << sredgarm(a, b); // Вывод результата
+
+        std::cout << endl;
+        return 0;
+
+}
